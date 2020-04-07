7fb4dcca8ce4d7d10b1105ae417be7c7ae55a7ba
im2nguyen <im2nguyen@users.noreply.github.com>
2020-04-07 13:46:32 -0700

    Update aks-cluster.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -56,6 +56,12 @@ output "kubernetes_cluster_name" {
 }
 
 # # Kubernetes provider
+# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
+# # It references the variables and resources provisioned in this file. 
+# # We recommend you put this in another file -- so you can have a more modular configuration.
+# # https://learn.hashicorp.com/terraform/kubernetes/provision-aks-cluster#optional-configure-terraform-kubernetes-provider
+# # To learn how to schedule deployments and services using the provider, go here: ttps://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes.
+
 # provider "kubernetes" {
 #   load_config_file = "false"
 
