559948f423681268bbe202e8c341f79fc71082aa
im2nguyen <im2nguyen@users.noreply.github.com>
2020-04-08 17:26:38 -0700

    Update aks-cluster.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -65,10 +65,10 @@ output "kubernetes_cluster_name" {
 # provider "kubernetes" {
 #   load_config_file = "false"
 
-#   host                   = "${azurerm_kubernetes_cluster.main.kube_config.0.host}"
-#   username               = "${azurerm_kubernetes_cluster.main.kube_config.0.username}"
-#   password               = "${azurerm_kubernetes_cluster.main.kube_config.0.password}"
-#   client_certificate     = "${base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)}"
-#   client_key             = "${base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)}"
-#   cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)}"
+#   host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
+#   username               = azurerm_kubernetes_cluster.main.kube_config.0.username
+#   password               = azurerm_kubernetes_cluster.main.kube_config.0.password
+#   client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
+#   client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
+#   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
 # }
