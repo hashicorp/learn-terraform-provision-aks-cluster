9ea23a28b5a1b66c6df775ddf90b8c9965f3203c
Tu Nguyen <im2nguyen@gmail.com>
2021-07-06 12:56:35 -0700

    Upgrade azure provider versions, remove dashboard - k8s 1.19+

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -35,12 +35,6 @@ resource "azurerm_kubernetes_cluster" "default" {
     enabled = true
   }
 
-  addon_profile {
-    kube_dashboard {
-      enabled = true
-    }
-  }
-
   tags = {
     environment = "Demo"
   }
--- versions.tf
+++ versions.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.42.0"
+      version = "2.66.0"
     }
   }
 
-  required_version = "~> 0.14"
+  required_version = ">= 0.14"
 }
 
