093ca5d5cf6af496798328c4d873953edadb3844
Brian McClain <brianmmcclain@gmail.com>
2023-08-03 18:32:05 -0400

    Update VM instance size

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   default_node_pool {
     name            = "default"
     node_count      = 2
-    vm_size         = "Standard_B2s"
+    vm_size         = "Standard_D2_v2"
     os_disk_size_gb = 30
   }
 
